78c295090909106bab687f314ccb103582fc5a9d
BN-Sambe <bihaag.nepaul@sambeconsulting.com>
2024-08-01 09:24:21 +0200

    added new scripts

--- deploy_pbip.ps1
+++ deploy_pbip.ps1
@@ -1,29 +1,17 @@
-# Parameters 
-$workspaceName = $env:WORKSPACE_NAME
-$pbipSemanticModelPath = $env:PBIP_SEMANTIC_MODEL_PATH
-$pbipReportPath = $env:PBIP_REPORT_PATH
-$currentPath = (Split-Path $MyInvocation.MyCommand.Definition -Parent)
-Set-Location $currentPath
+# Authenticate to Power BI service
+$tenantId = $env:TENANT_ID
+$clientId = $env:CLIENT_ID
+$clientSecret = $env:CLIENT_SECRET
+$workspaceId = $env:WORKSPACE_ID
+$filePath = $env:FILE_PATH
 
-# Download modules and install
-New-Item -ItemType Directory -Path ".\modules" -ErrorAction SilentlyContinue | Out-Null
-@("https://raw.githubusercontent.com/microsoft/Analysis-Services/master/pbidevmode/fabricps-pbip/FabricPS-PBIP.psm1"
-, "https://raw.githubusercontent.com/microsoft/Analysis-Services/master/pbidevmode/fabricps-pbip/FabricPS-PBIP.psd1") |% {
-    Invoke-WebRequest -Uri $_ -OutFile ".\modules\$(Split-Path $_ -Leaf)"
-}
-if(-not (Get-Module Az.Accounts -ListAvailable)) { 
-    Install-Module Az.Accounts -Scope CurrentUser -Force
-}
-Import-Module ".\modules\FabricPS-PBIP" -Force
+$secureClientSecret = ConvertTo-SecureString $clientSecret -AsPlainText -Force
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $secureClientSecret
 
-# Authenticate
-Set-FabricAuthToken -reset
+Connect-AzAccount -ServicePrincipal -Credential $credential -Tenant $tenantId
 
-# Ensure workspace exists
-$workspaceId = New-FabricWorkspace  -name $workspaceName -skipErrorIfExists
+# Import the .pbip file to the specified workspace
+Import-PowerBIFile -WorkspaceId $workspaceId -FilePath $filePath
 
-# Import the semantic model and save the item id
-$semanticModelImport = Import-FabricItem -workspaceId $workspaceId -path $pbipSemanticModelPath
-
-# Import the report and ensure its binded to the previous imported report
-$reportImport = Import-FabricItem -workspaceId $workspaceId -path $pbipReportPath -itemProperties @{"semanticModelId" = $semanticModelImport.Id}
+# Disconnect from Power BI service
+Disconnect-AzAccount
